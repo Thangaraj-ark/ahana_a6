1013e8e85ac52cb18fafb3b0e4df0ea8bd10e457
Jai-Kumaresh <jai@arkinfotec.com>
2018-09-10 19:28:33 +0530

    login role select box

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
     selector: 'app-login',
@@ -12,16 +13,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class LoginComponent implements OnInit {
     constructor(public router: Router, private http: HttpClient) {}
 
+	baseUrl = environment.baseUrl;
+	model: any = {};
+	allTenantLists = [];
+
 	ngOnInit() {
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Access-Control-Allow-Origin':  '*',
+				'x-domain-path': 'http://localhost:4200'
+			})
+		};
 		this.model.tenant_id = '';
-		this.http.get('http://hmsapi.ark/IRISORG/web/v1/default/get-tenant-list').toPromise().then(res => { // Success
+		this.http.get(this.baseUrl + 'IRISORG/web/v1/default/get-tenant-list', httpOptions).toPromise().then(res => { // Success
 			console.log(res);
+			if (res['org_sts']) {
+				for (let key in res['tenantList']) {
+					this.allTenantLists.push({
+						id: key,
+						role: res['tenantList'][key]
+					})
+				}
+			}
 		}, err => { // Error
 			console.log(err);
 		});
 	}
-
-	model: any = {};
 
 	onSubmit() {
 		// localStorage.setItem('isLoggedin', 'true');
@@ -29,11 +46,12 @@ export class LoginComponent implements OnInit {
 		// this.router.navigate(['dashboard']);
 		const httpOptions = {
 			headers: new HttpHeaders({
-				'Access-Control-Allow-Origin':  '*'
+				'Access-Control-Allow-Origin':  '*',
+				'x-domain-path': 'http://localhost:4200'
 			})
 		};
 		var self = this;
-		this.http.post('http://hmsapi.ark/IRISORG/web/v1/user/login', this.model).toPromise().then(res => { // Success
+		this.http.post(this.baseUrl + 'IRISORG/web/v1/user/login', this.model, httpOptions).toPromise().then(res => { // Success
 			console.log(res);
 			// if(res) {
 			// 	localStorage.setItem('curUserId', res[0].id);
