a8a4506eafee59bb4f537e98f487675ad58aa0d4
Jai-Kumaresh <jai@arkinfotec.com>
2018-09-10 12:21:59 +0530

    login form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-login',
@@ -9,11 +10,28 @@ import { routerTransition } from '../router.animations';
     animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
-    constructor(public router: Router) {}
+    constructor(public router: Router, private http: HttpClient) {}
 
-    ngOnInit() {}
+	ngOnInit() {
+		this.model.tenant_id = '';
+	}
 
-    onLoggedin() {
-        localStorage.setItem('isLoggedin', 'true');
-    }
+	model: any = {};
+
+	onSubmit() {
+		localStorage.setItem('isLoggedin', 'true');
+		console.log(this.model)
+		this.router.navigate(['dashboard']);
+		// var self = this;
+		// this.http.post('http://localhost:3000/employee-login', this.loginForm.value).toPromise().then(res => { // Success
+		// 	console.log(res);
+		// 	if(res) {
+		// 		localStorage.setItem('curUserId', res[0].id);
+		// 		self.router.navigate(['welcome']);
+		// 	}
+		// }, err => { // Error
+		// 	console.log(err);
+		// });
+	}
+
 }
